b60b8e782f9c3532aaa9a557112922a2907728c3
sunwooz <yangsunwoo@gmail.com>
2017-03-29 15:33:35 -0400

    generate users controller override for devise and permit attributes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   get 'posts/index'
 
-  devise_for :users
+  devise_for :users, :controllers => {:registrations => "users/registrations"}
 
   root to: 'posts#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

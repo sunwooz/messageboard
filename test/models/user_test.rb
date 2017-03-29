5c63af17b87998cff0ab2604842604ad04d6c554
sunwooz <yangsunwoo@gmail.com>
2017-03-29 16:49:51 -0400

    add user unit test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,19 @@
-require 'test_helper'
+# spec/models/user_spec.rb
 
-class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+require 'test_helper.rb'
+
+# Prefix instance methods with a '#'
+describe Contact do
+  it "is valid with first_name, last_name" do
+
+    user = User.create(first_name: 'Sunwoo', last_name: 'Yang')
+
+  end
+
+  it "is invalid without a firstname"
+  it "is invalid without a lastname"
+  it "is invalid without an email address"
+  it "is invalid with a duplicate email address"
+  it "returns a contact's full name as a string”
+
 end

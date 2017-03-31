90e62c42de65e7b83e17212b4445ea2034ee2ee9
sunwooz <yangsunwoo@gmail.com>
2017-03-31 18:07:07 -0400

    post link

--- client/app/bundles/posts/components/Post.jsx
+++ client/app/bundles/posts/components/Post.jsx
@@ -3,9 +3,11 @@ import React, { PropTypes, Component } from 'react';
 export default class Post extends React.Component {
   render() {
     var post = this.props.post;
+    var post_link = '/posts/' + post.id
     return (
       <div>
-        <h1>{post.title}</h1>
+        <h1><a href={post_link}>{post.title}</a></h1>
+        <p>Author: {post.user_id}</p>
         <p>{post.body}</p>
       </div>
     )

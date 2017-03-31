356a53d038295fc2a901e9e92e441cfb2de0ec62
sunwooz <yangsunwoo@gmail.com>
2017-03-31 18:06:56 -0400

    first last name allowed on devise

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   before_action :configure_account_update_params, only: [:update]
+  before_action :configure_sign_up_params, only: [:create]
 
   # GET /resource/sign_up
   # def new
@@ -36,12 +37,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params

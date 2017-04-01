a22de2e30926060579eab9744bcdea64dfd4e216
sunwooz <yangsunwoo@gmail.com>
2017-04-01 13:21:27 -0400

    add simplecov

--- Gemfile
+++ Gemfile
@@ -28,6 +28,7 @@ gem 'haml'
 gem 'react_on_rails'
 gem 'bootstrap-sass'
 gem 'faker'
+gem 'simplecov', :require => false, :group => :test
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- client/app/bundles/posts/components/Post.jsx
+++ client/app/bundles/posts/components/Post.jsx
@@ -4,6 +4,7 @@ export default class Post extends React.Component {
   render() {
     var post = this.props.post;
     var post_link = '/posts/' + post.id
+
     return (
       <div>
         <h1><a href={post_link}>{post.title}</a></h1>

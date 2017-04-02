92fdec64818906986a1b868a9028aff494597a59
sunwooz <yangsunwoo@gmail.com>
2017-04-02 13:46:50 -0400

    remove destroy buttons

--- client/app/bundles/posts/components/Post.jsx
+++ client/app/bundles/posts/components/Post.jsx
@@ -16,22 +16,36 @@ export default class Post extends React.Component {
     )
   }
 
+  // getUserName(user_id) {
+  //   $.get('/users',
+  //     {
+  //       post: post,
+  //       header: header,
+  //     }
+  //   ).done(function(data) {
+  //     this.refs.createpostbutton.close();
+  //     this.handleNewPost(data);
+  //   }.bind(this));
+  // }
+
   render() {
     var post = this.props.post;
     console.log(post);
     var post_link = '/posts/' + post.slug;
-    var buttons = '';
+    // var buttons = '';
     var current_user = this.props.current_user;
 
-    if (post.user_id == current_user.id) {
-      buttons = this.renderDestroyButton(post_link);
-    }
+    // if (post.user_id == current_user.id) {
+    //   buttons = this.renderDestroyButton(post_link);
+    // }
+
+    var user_id = post.user_id;
+    // this.getUserName(user_id)
     
     return (
       <div>
         <h1><a href={post_link}>{post.title}</a></h1>
         <p>Author: {post.user_id}</p>
-        { buttons }
         <div dangerouslySetInnerHTML={this.rawMarkup()}></div>
       </div>
     )

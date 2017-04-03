52130f94234275788962a64d481c28d0e9542fb8
sunwooz <yangsunwoo@gmail.com>
2017-04-02 20:20:55 -0400

    comments errors

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,9 +4,12 @@ class CommentsController < ApplicationController
     @comment = Comment.new(comment_params)
     if current_user
       @comment.user_id = current_user.id
-      @comment.save
+      if @comment.save
+        render json: @comment
+      else
+        render json: { errors: @comment.errors.full_messages }
+      end
     end
-    render json: @comment
   end
 
   def destroy

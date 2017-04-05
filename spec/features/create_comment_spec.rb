269b33d5f2a4a22cc0c538879ce798bca1064ebd
sunwooz <yangsunwoo@gmail.com>
2017-04-05 09:57:44 -0400

    add more tests

--- spec/features/create_comment_spec.rb
+++ spec/features/create_comment_spec.rb
@@ -15,7 +15,7 @@ feature "User creates a new Comment", js: true do
   }
 
   context 'while signed in' do
-    before(:all) do
+    before(:each) do
       visit '/'
       click_on 'Login'
       fill_in 'user[email]', with: 'yangsunwoo@gmail.com'
@@ -31,6 +31,24 @@ feature "User creates a new Comment", js: true do
       page.find('#submit-comment-modal-button').click
 
       expect(page).to have_content('test comment')
+    end
+
+    context 'when body is empty' do
+      it 'should display body cannot be empty error' do
+        visit '/'
+        page.click_link 'Post title'
+        page.find('button#open-comment-modal-button').click
+        page.find('#submit-comment-modal-button').click
+        expect(page).to have_content("Body can't be blank")
+      end
+    end
+  end
+
+  context 'while signed out' do
+    it 'should not have create comment button' do
+      visit '/'
+      page.click_link 'Post title'
+      page.should_not have_css('#open-comment-modal-button')
     end
   end
 
